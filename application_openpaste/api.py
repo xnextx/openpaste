0e150c13bf961f062a74b8094af34c1f05d702fb
Piotr Majewski <piotr@placenet.pl>
2016-01-31 22:59:30 +0100

    -Added view "add inset" -Added generating random string in private inset

--- application_openpaste/api.py
+++ application_openpaste/api.py
@@ -2,21 +2,43 @@ from rest_framework import viewsets, permissions, filters
 from application_openpaste.models import *
 from .serializers import InsetSerializer
 from rest_framework.permissions import DjangoModelPermissions, IsAuthenticated
-
-
+import datetime
+import random, string
+from django.contrib.auth.models import User
 
 class InsetViewSet(viewsets.ModelViewSet):
     queryset = Inset.objects.all()
     model = Inset
     serializer_class = InsetSerializer
-    permission_classes = (DjangoModelPermissions, )
+    # permission_classes = (DjangoModelPermissions, )
+
+
+    def random_string(self, length):
+        hash = ""
+        while(True):
+            generate_string = ''.join(random.choice(string.lowercase) for i in range(length))
+
+            if(Inset.objects.filter(url_private=generate_string).count() == 0):
+                hash = generate_string
+                break
+
+        return hash
+
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        if(self.request.user.id != None):
+            serializer.save(owner=self.request.user, date_added=datetime.datetime.today(), url_private=self.random_string(20))
+        else:
+            serializer.save(date_added=datetime.datetime.today(), url_private=self.random_string(20))
 
-    def perform_update(self, serializer):
-        serializer.save(owner=self.request.user)
+    # def perform_update(self, serializer):
+    #     if(self.request.user.id != None):
+    #         serializer.save(owner=self.request.user, date_added=datetime.datetime.today())
+    #     else:
+    #         serializer.save(date_added=datetime.datetime.today())
 
-    def get_queryset(self):
-        queryset = Inset.objects.filter(owner=self.request.user)
-        return queryset
+
+    # Developer mode :D
+    # def get_queryset(self):
+    #     queryset = Inset.objects.filter(private=False)
+    #     return queryset
--- static/js/logic.js
+++ static/js/logic.js
@@ -6,7 +6,42 @@ var Application = angular.module('Application', []).config(function($httpProvide
 
 
 Application.controller('Paste_system', function ($scope, $http, $timeout) {
-    $scope.variable = "Jakaś treść";
+
+    $scope.state_alert = false;
+
+/*
+    Alert system:
+*/
+    var stop_alert = function (){
+        $scope.state_alert = false;
+        console.log("dwa");
+    };
+    var run_alert = function (content, type, show_time){
+        $scope.state_alert = true;
+        $scope.alert = content;
+        $scope.alert_type = type;
+        $timeout(function(){stop_alert();}, show_time);
+    };
+
+/*
+    End Alert system:
+*/
+    $scope.send_inset = function(content, priv) {
+        console.log("wysłano");
+
+        var success = function(response){
+            run_alert("Wysłano...", "success", 1500)
+        };
+        var error = function(response){
+            run_alert("Nastąpił błąd przy wysyłaniu.", "error", 1500)
+        };
+        $http.post("/api/v1/Insets/",
+            {
+                "content": content,
+                "private": priv
+
+            }).then(success, error);
+    }
 
 
   });
