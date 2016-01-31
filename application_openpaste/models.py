97c03d043831ad9be976fd805197326cf3157dbf
Piotr Majewski <piotr@placenet.pl>
2016-01-31 19:27:52 +0100

    -add static files -add templates initial files

--- application_openpaste/models.py
+++ application_openpaste/models.py
@@ -1,3 +1,12 @@
+#-*-coding:utf-8-*-
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+#Inset-Wklejka
+class Inset(models.Model):
+    owner = models.ForeignKey(User)
+    content = models.CharField(verbose_name="Zawartosc", max_length=1000)
+
+    def __unicode__(self):
+        return u"%s" % (self.owner)
